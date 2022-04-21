5dded5a408791fae66364ba76c531a301b10c59a
syruponwaffles <nicholaskor@hotmail.com>
2022-04-21 07:26:15 -0600

    added offset for map drawing,improved selection to account for centering, turned draw tiles in a function (drawMap()),removed unnecessary calculations using halftile instead of sizeTile / 2

--- conversion.c
+++ conversion.c
@@ -1,9 +1,21 @@
+/**
+ * @brief converts input x and y from cartesian to isometic
+ * 
+ * @param x x coordinate pointer
+ * @param y y coordinate pointer
+ */
 void cti(float* x, float* y){
     float nx = *x + *y;
     float ny = -0.5 * *x + 0.5 * *y;
     *x = nx / 2;
     *y = ny / 2;
 }
+/**
+ * @brief converts input x and y from isometric to cartesian
+ * 
+ * @param x x coordinate pointer
+ * @param y y coordinate pointer
+ */
 void itc(float* x, float* y){
     float nx = (0.5 * *x  * 2) - *y  * 2;
     float ny = (0.5 * *x  * 2) + *y  * 2;
--- main.c
+++ main.c
@@ -4,35 +4,68 @@
 #include "math.h"
 
 #define tileSize 64
+#define halfTile tileSize / 2
 #define tileCenter tileSize * 6.015625
+/**
+ * @brief simple coordinate system with two floats
+ * 
+ */
 typedef struct coordinates
 {
     float x; 
     float y; 
 }coordinates;
 
+/**
+ * @brief map data class. contains a textures list floor map, prop map and collision map
+ * 
+ */
 typedef struct map
 {
     Texture2D key[16];
     int floor[32][32];
     int props[32][32];
+    int collision[32][32];
+
     
 }map;
 
+void drawMap(map level, int sizex, int sizey, coordinates offset){
+    for (int fy = 0; fy < sizey; fy++)
+            {
+                for (int fx = sizex; fx > 0; fx--)
+                {
+                    coordinates tilecoord = {fx,fy}; 
+                    cti(&tilecoord.x,&tilecoord.y);
+                    // Vector2 tile = {tilecoord.x*tileSize + ((1280 / 2) - 4.5 * tileSize), tilecoord.y*tileSize+ (720/2 - tileSize)};
+                    Vector2 tile = {(tilecoord.x*tileSize -tileSize + tileCenter), tilecoord.y*tileSize + 360};
+                    // Vector2 tile = {(tilecoord.x*tileSize), tilecoord.y*tileSize};
+                    DrawTextureEx(level.key[level.floor[fy + (int)offset.y][fx - 1 + (int)offset.x] - 1], tile, 0, 2, WHITE);
+                    tile.y -= halfTile;
+                    DrawTextureEx(level.key[level.props[fy][fx - 1] - 1], tile, 0, 2, WHITE);
+                    // DrawTextureEx(concrete, tile, 0, 2, WHITE);
+
+                }
+                
+            }
+}
 int main(void)
 {
     const int screenWidth = 1280;
     const int screenHeight = 720;
+    coordinates mapOffset = {0,0};
+
     InitWindow(screenWidth, screenHeight, "Isometric World");
 
     map test = {{LoadTexture("resources/waterTile.png"),
      LoadTexture("resources/concreteTile.png"),
      LoadTexture("resources/tableTile.png"),
      LoadTexture("resources/tableEndTile1.png"),
-     LoadTexture("resources/tableEndTile2.png")},{{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-                          {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-                          {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-                          {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+     LoadTexture("resources/tableEndTile2.png"),
+     LoadTexture("resources/swatCharacter.png")},{{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+                          {1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+                          {1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+                          {1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
                           {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
                           {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
                           {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
@@ -48,9 +81,9 @@ int main(void)
 
                          {{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
                           {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-                          {0,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0},
-                          {0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0},
-                          {0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                          {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                          {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                          {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
                           {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
                           {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
                           {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
@@ -68,48 +101,36 @@ int main(void)
     {
         // printf("%i %i \n", GetMouseX(),GetMouseY());
 
-        coordinates mouse = {(GetMouseX() * 1.0) / (tileSize / 2), GetMouseY() * 1.0 / (tileSize / 2)};
+        coordinates mouse = {((GetMouseX() + tileSize - tileCenter) * 1.0) / (halfTile), (GetMouseY() * 1.0 - 360) / (halfTile)};
         itc(&mouse.x,&mouse.y);
-        coordinates selectedcoord = {roundf(mouse.x / 2),roundf(mouse.y / 2)};
-        printf("%f %f \n", selectedcoord.x,selectedcoord.y);
+        coordinates selectedcoord = {roundf(mouse.x / 2),roundf(mouse.y / 2) - 1};
+        printf("%i %i \n", (int)selectedcoord.x,(int)selectedcoord.y);
         
         cti(&selectedcoord.x,&selectedcoord.y);
-        Vector2 selected = {selectedcoord.x*tileSize, selectedcoord.y*tileSize};
-        // Vector2 selected = {selectedcoord.x*tileSize - tileSize / 2,selectedcoord.y*tileSize};
+        Vector2 selected = {(selectedcoord.x*tileSize -tileSize + tileCenter), selectedcoord.y*tileSize + 360};
+        selected.y -= halfTile;
+
+        // Vector2 selected = {selectedcoord.x*tileSize - halfTile,selectedcoord.y*tileSize};
 
         BeginDrawing();
-
+            
             ClearBackground(RAYWHITE);
-            for (int fy = 0; fy < 8; fy++)
-            {
-                for (int fx = 8; fx > 0; fx--)
-                {
-                    coordinates tilecoord = {fx,fy}; 
-                    cti(&tilecoord.x,&tilecoord.y);
-                    // Vector2 tile = {tilecoord.x*tileSize + ((1280 / 2) - 4.5 * tileSize), tilecoord.y*tileSize+ (720/2 - tileSize)};
-                    Vector2 tile = {(tilecoord.x*tileSize -tileSize + tileCenter), tilecoord.y*tileSize + 360};
-                    // Vector2 tile = {(tilecoord.x*tileSize), tilecoord.y*tileSize};
-                    DrawTextureEx(test.key[test.floor[fy][fx - 1] - 1], tile, 0, 2, WHITE);
-                    tile.y -= tileSize / 2;
-                    DrawTextureEx(test.key[test.props[fy][fx - 1] - 1], tile, 0, 2, WHITE);
-
-                    // DrawTextureEx(concrete, tile, 0, 2, WHITE);
-
-                }
-                
-            }
-            DrawTextureEx(test.key[0], selected, 0, 2, WHITE);
+            drawMap(test,8,8, mapOffset);
+            DrawTextureEx(test.key[5], selected, 0, 2, WHITE);
             
             DrawFPS(0,0);
         EndDrawing();
         
         //----------------------------------------------------------------------------------
     }
-
+    
     // De-Initialization
     //--------------------------------------------------------------------------------------
-    UnloadTexture(test.key[0]);
-    UnloadTexture(test.key[1]);       // Texture unloading
+    for (int i = 0; i < 4; i++)
+    {
+       UnloadTexture(test.key[i]);
+    }
+           // Texture unloading
 
     CloseWindow();                // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
