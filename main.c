1983c2d1f9c58ad2cd2f28f20139cd6038194dc5
syruponwaffles <nicholaskor@hotmail.com>
2022-04-22 18:10:59 -0600

    add highlight

--- main.c
+++ main.c
@@ -36,8 +36,9 @@ typedef struct map
  * @param sizex how many tiles to render on the x axis
  * @param sizey how many tiles to render on the y axis
  * @param offset coordinate x and y offset
+ * @param layer 1: floor, 2: props, 3:collision
  */
-void drawMap(map level, int sizex, int sizey, coordinates offset){
+void drawMap(map level, int sizex, int sizey, coordinates offset, char layer){
     for (int fy = 0; fy < sizey; fy++)
             {
                 for (int fx = sizex; fx > 0; fx--)
@@ -47,14 +48,39 @@ void drawMap(map level, int sizex, int sizey, coordinates offset){
                     // Vector2 tile = {tilecoord.x*tileSize + ((1280 / 2) - 4.5 * tileSize), tilecoord.y*tileSize+ (720/2 - tileSize)};
                     Vector2 tile = {(tilecoord.x*tileSize -tileSize + tileCenter), tilecoord.y*tileSize + 360};
                     // Vector2 tile = {(tilecoord.x*tileSize), tilecoord.y*tileSize};
-                    DrawTextureEx(level.key[level.floor[fy + (int)offset.y][fx - 1 + (int)offset.x] - 1], tile, 0, 2, WHITE);
-                    tile.y -= halfTile;
-                    DrawTextureEx(level.key[level.props[fy][fx - 1] - 1], tile, 0, 2, WHITE);
+                    switch (layer)
+                    {
+                    case 1:
+                            DrawTextureEx(level.key[level.floor[fy + (int)offset.y][fx - 1 + (int)offset.x] - 1], tile, 0, 2, WHITE);
+                        break;
+                    
+                    case 2:
+                            tile.y -= halfTile;
+                            DrawTextureEx(level.key[level.props[fy+ (int)offset.y][fx - 1 + (int)offset.x] - 1], tile, 0, 2, WHITE);
+                        break;
+                    default:
+                        break;
+                    }
                     // DrawTextureEx(concrete, tile, 0, 2, WHITE);
 
                 }
                 
             }
+}
+/**
+ * @brief Get the tile number you are hovering over
+ * 
+ * @return coordinate
+ */
+Vector2 getMouseTilePosition(){
+    coordinates mouse = {((GetMouseX() + tileSize - tileCenter) * 1.0) / (halfTile), (GetMouseY() * 1.0 - 360) / (halfTile)};
+    itc(&mouse.x,&mouse.y);
+    coordinates selectedcoord = {roundf(mouse.x / 2) - 1,roundf(mouse.y / 2) - 1};
+    printf("%i %i \n", (int)selectedcoord.x,(int)selectedcoord.y);
+    
+    cti(&selectedcoord.x,&selectedcoord.y);
+    Vector2 selected = {(selectedcoord.x*tileSize -halfTile + tileCenter), selectedcoord.y*tileSize + 360};
+    return selected;
 }
 int main(void)
 {
@@ -69,7 +95,8 @@ int main(void)
      LoadTexture("resources/tableTile.png"),
      LoadTexture("resources/tableEndTile1.png"),
      LoadTexture("resources/tableEndTile2.png"),
-     LoadTexture("resources/swatCharacter.png")},{{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+     LoadTexture("resources/swatCharacter.png"),
+     LoadTexture("resources/highlight.png"),},{{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
                           {1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
                           {1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
                           {1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
@@ -87,9 +114,9 @@ int main(void)
                           {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}},
 
                          {{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-                          {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-                          {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-                          {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                          {0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                          {0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                          {0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
                           {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
                           {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
                           {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
@@ -103,27 +130,24 @@ int main(void)
                           {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
                           {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
     }};
-
+    Vector2 selected = getMouseTilePosition();
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         // printf("%i %i \n", GetMouseX(),GetMouseY());
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) selected = getMouseTilePosition();
 
-        coordinates mouse = {((GetMouseX() + tileSize - tileCenter) * 1.0) / (halfTile), (GetMouseY() * 1.0 - 360) / (halfTile)};
-        itc(&mouse.x,&mouse.y);
-        coordinates selectedcoord = {roundf(mouse.x / 2),roundf(mouse.y / 2) - 1};
-        printf("%i %i \n", (int)selectedcoord.x,(int)selectedcoord.y);
         
-        cti(&selectedcoord.x,&selectedcoord.y);
-        Vector2 selected = {(selectedcoord.x*tileSize -tileSize + tileCenter), selectedcoord.y*tileSize + 360};
-        selected.y -= halfTile;
+        // selected.y -= halfTile;
 
         // Vector2 selected = {selectedcoord.x*tileSize - halfTile,selectedcoord.y*tileSize};
 
         BeginDrawing();
             
             ClearBackground(RAYWHITE);
-            drawMap(test,8,8, mapOffset);
-            DrawTextureEx(test.key[5], selected, 0, 2, WHITE);
+            drawMap(test,8,8, mapOffset, 1);
+            DrawTextureEx(test.key[6], selected, 0, 2, WHITE);
+            drawMap(test,8,8, mapOffset, 2);
+
             
             DrawFPS(0,0);
         EndDrawing();
