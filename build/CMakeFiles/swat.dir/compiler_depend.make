64cdfc1c9fc7326fd97685ffe2b16aec4ddc514c
SyrupOnWaffles <nicholaskor@hotmail.com>
2022-04-29 10:02:15 -0600

    ffs remove build

--- build/CMakeFiles/swat.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for swat.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/swat.dir/conversion.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/swat.dir/conversion.c.o: \
- /home/syrup/Desktop/Programming/C/swat/conversion.c \
- /usr/include/stdc-predef.h
